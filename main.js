0c4d22bf1045111493eb8f7d793c74c5263663d0
ccantoran <102254956+ccantoran@users.noreply.github.com>
2022-04-19 23:44:32 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,22 +1,25 @@
 const mainMenu = document.querySelector('.mainMenu')
 const openMenu = document.querySelector('.openMenu')
+const closeMenu = document.querySelector('.closeMenu')
 
 
 document.querySelector('.openMenu').addEventListener('click', show);
 document.querySelector('.closeMenu').addEventListener('click', close);
-document.querySelector('.firstli').addEventListener('click', close);
-document.querySelector('.secondli').addEventListener('click', close);
-document.querySelector('.thirdli').addEventListener('click', close);
-document.querySelector('.fourthli').addEventListener('click', close);
 
 function show(){
-    mainMenu.style.display = 'block';
+    mainMenu.style.display = 'flex';
     openMenu.style.display = 'none';
+    document.querySelector('.firstli').addEventListener('click', close);
+    document.querySelector('.secondli').addEventListener('click', close);
+    document.querySelector('.thirdli').addEventListener('click', close);
+    document.querySelector('.fourthli').addEventListener('click', close);
+    document.querySelector('.fifthli').addEventListener('click', close);
 }
 function close(){
-    mainMenu.style.display = 'none';
-    openMenu.style.display = 'flex'
+    mainMenu.style.display = "none";
+    openMenu.style.display = "flex";
 }
+
 
 
 
